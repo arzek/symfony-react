3448018f6478605722265311bab0ab55ca6c16e8
Artem <arzek2015@gmail.com>
2018-04-10 22:58:00 +0300

    win fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,6 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+mix.setPublicPath('web');
 mix.react('frontend/js/app.js', 'web/js')
    .sass('frontend/sass/app.scss', 'web/css');
